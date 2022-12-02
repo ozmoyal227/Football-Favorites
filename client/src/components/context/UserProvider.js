4f2108dd7fb0a7819e0779b6bbdc495fadc29e93
ozmoyal227 <moyal.oz.18@gmail.com>
2022-12-02 11:55:20 +0200

    updating scripts

--- client/src/components/context/UserProvider.js
+++ client/src/components/context/UserProvider.js
@@ -1,10 +1,24 @@
-import React, { useContext, createContext, useState } from "react";
+import React, { useContext, createContext, useState, useEffect } from "react";
 import constants from "../../constants";
+// import jwt_decode from "jwt-decode";
 
 const UserContext = createContext({});
 
 export function UserProvider({ children }) {
+    // const [isExpired, setIsExpired] = useState(true);
     const currUser = JSON.parse(localStorage.getItem('user'));
+
+
+    // useEffect(() => {
+    //     const tokenExpirationTime = currUser && jwt_decode(currUser.token.split(" ")[1]).exp * 1000;
+    //     if (Date.now() >= tokenExpirationTime)
+    //         setIsExpired(true);
+    //     else
+    //         setIsExpired(false);
+
+
+    // }, [])
+
 
     const [user, setUser] = useState(
         currUser ?
